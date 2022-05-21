d0565883f04bce965473bb3cc1cc404bfd15db54
Sam <105581592+Exaeous@users.noreply.github.com>
2022-05-21 16:46:13 +0530

    Solution for Checkpoint 1

---
+++ Checkpoint 1/Checkpoint 1.playground/Contents.swift
@@ -0,0 +1,6 @@
+import Cocoa
+
+let temperatureInCelsius = 32
+let temperatureInFahrenheit = (temperatureInCelsius * 9 / 5) + 32
+
+print("The temperature in celsius is \(temperatureInCelsius) and fahrenheit is \(temperatureInFahrenheit)")
